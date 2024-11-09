2abe87b5f2d95ee13f33f673ebc59003aa003a04
David_CB <cruzdavid158@gmail.com>
2024-11-09 13:21:28 -0600

    Primer commit proyectos

--- proyectos/models.py
+++ proyectos/models.py
@@ -11,6 +11,11 @@ class Proyectos(models.Model):
     estatus = models.BooleanField(default=True)
     total = models.DecimalField(max_digits=10, decimal_places=2,null=False,default=Decimal('0.00'))
     iva = models.DecimalField(max_digits=10, decimal_places=2,null=False,default=Decimal('0.00'))
+    progreso = models.PositiveIntegerField(default=0) 
+    fecha_creacion = models.DateField(auto_now_add=True)  
+    fecha_fin_estimada = models.DateField(null=True, blank=True)  
+    presupuesto_estimado = models.DecimalField(max_digits=12, decimal_places=2, null=True, blank=True)
+    ganancia_estimada = models.DecimalField(max_digits=12, decimal_places=2, null=True, blank=True)
     slug = models.SlugField(unique=True)
 
     def save(self, *args, **kwargs):
--- proyectos/tests.py
+++ proyectos/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from django.test import TestCase
+from django_tenants.utils import tenant_context
+from clientes.models import Cliente
+from proyectos.models import Proyectos  
 
-# Create your tests here.
+class TenantTestCase(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        # Crea un tenant de prueba
+        cls.tenant = Cliente.objects.create(
+            dominio='prueba.localhost',
+            schema_name='prueba',
+            # Rellena los demás campos requeridos por el modelo
+        )
+        cls.tenant.save()
+
+    def setUp(self):
+        # Esto asegura que cada prueba se ejecute en el contexto del inquilino
+        self.tenant_context = tenant_context(self.tenant)
+        self.tenant_context.__enter__()
+
+    def tearDown(self):
+        # Sal del contexto al final de cada prueba
+        self.tenant_context.__exit__(None, None, None)
+
+    def test_proyecto_creation(self):
+        # Ejemplo de prueba en el contexto del inquilino
+        proyecto = Proyectos.objects.create(
+            proyecto = "Proyecto de Prueba",
+            clave_proyecto = "Descripción del proyecto de prueba",
+            # Agrega otros campos necesarios
+        )
+        self.assertEqual(proyecto.proyecto, "Proyecto de Prueba")
