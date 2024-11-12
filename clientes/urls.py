cf1fc177327de86616d93c40e45c43f130facc8e
David_CB <cruzdavid158@gmail.com>
2024-11-11 21:08:23 -0600

    Update

--- clientes/urls.py
+++ clientes/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'clientes'
 
 urlpatterns = [
-    path("registro-clientes",views.crear_cliente, name = "crear_cliente"),
+    path("",views.crear_cliente, name = "crear_cliente"),
     # path("",pagina_principal, name="pagina_principal"),
 ]
--- usuarios/urls.py
+++ usuarios/urls.py
@@ -7,8 +7,8 @@ app_name = 'usuarios'
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
-    path('accounts/login/', auth_views.LoginView.as_view(template_name='usuarios/login.html',), name='login'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', auth_views.LoginView.as_view(template_name='usuarios/login.html',), name='login'),
+    path('logout/', views.logout_view, name='logout'),
     path('password_change/', auth_views.PasswordChangeView.as_view(template_name='usuarios/password_change.html',success_url=reverse_lazy('usuarios:password_change_done')), name='password_change'),    
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='usuarios/password_change_done.html'), name='password_change_done'),
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='usuarios/password_reset.html'), name='password_reset'),
