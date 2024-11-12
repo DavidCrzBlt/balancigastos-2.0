8a187367cdb4e7300ca76dd5c09e293799a21b39
David_CB <cruzdavid158@gmail.com>
2024-11-11 19:50:31 -0600

    Update pagina principal

--- clientes/urls.py
+++ clientes/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
-from .views import pagina_principal, crear_cliente
+
 
 app_name = 'clientes'
 
 urlpatterns = [
-    path("registro-clientes",crear_cliente, name="crear_cliente"),
+    path("registro-clientes",views.crear_cliente, name = "crear_cliente"),
     # path("",pagina_principal, name="pagina_principal"),
 ]
